1557378fff4a79e7e3ff406ba6a79a91f2fcdac5
Peter <schmidt.multimedia@gmail.com>
2021-06-17 09:24:16 +0200

    Klasse Block + constructor()

---
+++ block.js
@@ -0,0 +1,18 @@
+
+// Klasse Block
+class Block{
+
+    // Konstruktor + Parameter
+    constructor(timestamp,lastHash,hash,data){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.hash = hash;
+        this.data = data;
+    }
+
+
+
+
+
+
+}
