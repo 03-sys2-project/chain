88052457773dd4836b79d46d29eebd1615d06af9
mwcomputer <kolbenfresser5@googlemail.com>
2021-06-17 09:23:39 +0200

    next day

---
+++ block.js
@@ -0,0 +1,16 @@
+class Block{
+
+constructor(timestamp,lastHash,hash,data){
+    this.timestamp = timestamp;
+    this.lastHash = lastHash;
+    this.hash = hash;
+    this.data = data;
+
+}
+
+
+
+
+
+
+}
