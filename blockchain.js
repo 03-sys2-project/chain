31a8df714edf09f8c4de5e2544fe3c35b64c7be1
Peter <schmidt.multimedia@gmail.com>
2021-06-18 12:02:31 +0200

    + class Blockchain / constructor / dev-test

---
+++ blockchain.js
@@ -0,0 +1,20 @@
+const Block = require("./block");
+
+
+class Blockchain{
+
+    constructor(){
+        this.chain = [Block.genesis()];  // Am Anfang war ...
+    }
+
+
+
+
+
+
+
+
+
+} // EoC
+
+module.exports = Blockchain;
--- dev-test.js
+++ dev-test.js
@@ -1,10 +1,15 @@
 const Block = require("./block");
+const Blockchain = require("./blockchain");
 
 // const block = new Block("time","hash1","hash2","data");
 // console.log(block.toString());
 
 // console.log(Block.genesis().toString());
 
-const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
-console.log(testBlock.toString());
+// const testBlock = Block.mineBlock(Block.genesis(),"testDaten");
+// console.log(testBlock.toString());
+
+const blockchain =  new Blockchain();
+console.log(blockchain);
+
 
